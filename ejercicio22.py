16362c55c4b5ef5033b70c94ba7f1289cd7b0f42
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-09 22:39:41 -0500

    Add files via upload

---
+++ ejercicio22.py
@@ -0,0 +1,7 @@
+numero  =  int ( input ( "ingrese un número entero:" ))
+si numero <  0 :
+    print ( "el número es negativo" )
+elif  numero  ==  0 :
+    print ( "el número es neutro" )
+otra cosa :
+    print ( "el número es positivo" )
